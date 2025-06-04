7a25ad6c47d6fc4a38b04ed179feedf1d8b769ee
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-06-04 17:09:33 +0100

    Register form input useState implemented

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
 export default function LandingPage() {
+    const nav = useNavigate();
+
     return(
-        <h1>Landing page!</h1>
+        <>
+            <h1>Welcome to xats!</h1>
+            <div>
+                <button onClick={() => {nav('/register')}}>Register</button>
+                <br />
+                <button>Login</button>
+            </div>
+        </>
     )
 }
--- frontend/src/pages/RegisterForm.tsx
+++ frontend/src/pages/RegisterForm.tsx
@@ -1,5 +1,90 @@
+import { useState } from "react"
+
 export default function RegisterForm() {
+    const [inputs, setInputs] = useState({
+        username: "",
+        password:"",
+        profileImgUrl:"",
+        firstName:"",
+        lastName:"",
+        email:"",
+        dateOfBirth: undefined
+        })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs({...inputs, [name]: value})
+    }
+
     return(
-        <h1>Register Form</h1>
+        <>
+            <form>
+                <label>Username</label>
+                <br />
+                <input 
+                    type="string"
+                    name="username"
+                    value={inputs.username}
+                    onChange={handleChange}
+                 />
+                <br />
+                <label>Password</label>
+                <br />
+                <input 
+                    type="password"
+                    name="password"
+                    value={inputs.password}
+                    onChange={handleChange}
+                 />
+                <br />
+                <label>Profile Picture URL</label>
+                <br />
+                <input 
+                    type="string"
+                    name="profileImgUrl"
+                    value={inputs.profileImgUrl}
+                    onChange={handleChange}
+                 />
+                <br />
+                <label>First Name</label>
+                <br />
+                <input 
+                    type="string"
+                    name="firstName"
+                    value={inputs.firstName}
+                    onChange={handleChange}
+                 />
+                <br />
+                <label>Last Name</label>
+                <br />
+                <input 
+                    type="string"
+                    name="lastName"
+                    value={inputs.lastName}
+                    onChange={handleChange}
+                 />
+                <br />
+                <label>Email</label>
+                <br />
+                <input 
+                    type="string"
+                    name="email"
+                    value={inputs.email}
+                    onChange={handleChange}
+                 />
+                <br />
+                <label>Date Of Birth</label>
+                <br />
+                <input 
+                    type="date"
+                    name="dateOfBirth"
+                    value={inputs.dateOfBirth}
+                    onChange={handleChange}
+                 />
+                
+                 
+            </form>
+        </>
     )
 }
