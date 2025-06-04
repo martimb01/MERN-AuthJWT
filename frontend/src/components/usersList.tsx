28b9821ae91d2a6dff1b89e348f7fbbed180f1c3
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-06-04 20:05:37 +0100

    Built userList Component, my refactor the function to a standalone helperFunction file, gonna work on the user homepage now

---
+++ frontend/src/components/usersList.tsx
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+interface UserI {
+    firstName: string,
+    lastName:string
+}
+
+export default function usersList () {
+    const [usersArray, setUsersArray] = useState<UserI[]>([])
+
+    const fetchAllUsers = async () => {
+        try {
+            await axios
+            .get('http://localhost:4000/user/getAll')
+            .then(function(response){
+                console.log('Successful axios get!')
+                setUsersArray(response.data.users)
+            })
+            .catch(function(error){
+                console.log('Successful axios get but somethings gone wrong in the backend')
+                console.error(error)
+            })
+            
+        } catch (error) {
+            console.log('Axios get did not work!', error)
+        }
+    }
+
+    useEffect(() => {
+        fetchAllUsers()
+    } ,[])
+    return (
+        <>
+        {usersArray.map((user, index) => {
+            return (
+                <li key={index}>
+                    {user.firstName}{user.lastName}
+                    
+                </li>
+            )
+        })}
+        </>
+    )
+}
